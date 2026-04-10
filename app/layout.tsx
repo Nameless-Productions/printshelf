dcd00bc51d47a9ae0070a45f1dcbf42dac48d76d
thenamelessdev <nameless@thenamelessdev.com>
2026-04-10 21:40:15 +0200

    feat: add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./globals.css"
+import Link from 'next/link'
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
+  const linkClass = "font-bold";
   return (
     <html lang='en'>
       <body className='bg-neutral-900'>
-        <main className='m-1 prose text-neutral-100 prose-headings:text-neutral-100'>
+        <nav className='flex text-neutral-100 border-b p-2 items-center space-x-3'>
+          <Link href="/" className='text-2xl font-bold'>PrintShelf</Link>
+          <Link href="/" className={linkClass}>Home</Link>
+        </nav>
+        <main className='prose m-1 text-neutral-100 prose-headings:text-neutral-100'>
           {children}
         </main>
       </body>
