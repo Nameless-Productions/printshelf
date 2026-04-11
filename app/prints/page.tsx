c26586f444d16beee6052ee442be84f49f1d76ae
thenamelessdev <nameless@thenamelessdev.com>
2026-04-11 17:49:28 +0200

    update: make prints page wrapped in a server compontnt

--- app/prints/page.tsx
+++ app/prints/page.tsx
@@ -1,13 +1,9 @@
-"use client";
+import React from 'react'
+import PrintsPage from './PrintsPage'
 
-import React, { useState } from 'react'
-import NewPrintModal from './NewPrintModal';
-
-export default function PrintsPage() {
-    const [isOpen, setIsOpen] = useState(false);
+export default function PrintsPageServer() {
   return (<>
-    <button className='btn-green' onClick={() => setIsOpen(true)}>New print</button>
-
-    {isOpen && <NewPrintModal closeBtnOnClick={() => setIsOpen(false)}/>}
+    <PrintsPage />
+    
   </>)
 }
