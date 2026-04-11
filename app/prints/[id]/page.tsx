1ff0e1bfce7137c20ccc8e16c384540852c8ac63
thenamelessdev <nameless@thenamelessdev.com>
2026-04-11 21:41:34 +0200

    feat: routes and buttons to download files

--- app/prints/[id]/page.tsx
+++ app/prints/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getPrint } from '@/lib/prints/getPrint';
 import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import React from 'react'
 
@@ -11,6 +12,9 @@ export default async function PrintViewerPage({ params }: { params: { id: number
     <h3>{print.title}</h3>
     <p><b>Description:</b> <br />{print.description}</p>
     <p><b>Info:</b> <br />{print.info}</p>
+    <Link href={`/prints/${id}/gcode`}><button className='btn-green mb-3'>GCODE file</button></Link>
+    <br />
+    <Link href={`/prints/${id}/stl`}><button className='btn-green'>STL file</button></Link>
     <br />
     <b>Images: </b>
     {
